5ee0719fddc3ed08baab0176324211764f3ee694
ThisIsSyr <real_syr@163.com>
2026-02-04 11:38:21 +0800

    Refactor wavelet filter creation and improve padding

--- mmseg/models/uda/wtconv.py
+++ mmseg/models/uda/wtconv.py
@@ -1,48 +1,58 @@
+# mmseg/models/uda/wtconv.py
+
 import pywt
-import pywt.data
 import torch
 from torch import nn
 from torch.autograd import Function
 import torch.nn.functional as F
 
-# 论文：Wavelet Convolutions for Large Receptive Fields
-# 论文地址：https://arxiv.org/pdf/2407.05848
 
-def create_wavelet_filter(wave, in_size, out_size, type=torch.float):
+def create_wavelet_filter(wave, in_size, out_size, dtype=torch.float32):
     w = pywt.Wavelet(wave)
-    dec_hi = torch.tensor(w.dec_hi[::-1], dtype=type)
-    dec_lo = torch.tensor(w.dec_lo[::-1], dtype=type)
-    dec_filters = torch.stack([dec_lo.unsqueeze(0) * dec_lo.unsqueeze(1),
-                               dec_lo.unsqueeze(0) * dec_hi.unsqueeze(1),
-                               dec_hi.unsqueeze(0) * dec_lo.unsqueeze(1),
-                               dec_hi.unsqueeze(0) * dec_hi.unsqueeze(1)], dim=0)
 
+    # decomposition filters
+    dec_hi = torch.tensor(w.dec_hi[::-1], dtype=dtype)
+    dec_lo = torch.tensor(w.dec_lo[::-1], dtype=dtype)
+    dec_filters = torch.stack([
+        dec_lo.unsqueeze(0) * dec_lo.unsqueeze(1),  # LL
+        dec_lo.unsqueeze(0) * dec_hi.unsqueeze(1),  # LH
+        dec_hi.unsqueeze(0) * dec_lo.unsqueeze(1),  # HL
+        dec_hi.unsqueeze(0) * dec_hi.unsqueeze(1),  # HH
+    ], dim=0)
     dec_filters = dec_filters[:, None].repeat(in_size, 1, 1, 1)
 
-    rec_hi = torch.tensor(w.rec_hi[::-1], dtype=type).flip(dims=[0])
-    rec_lo = torch.tensor(w.rec_lo[::-1], dtype=type).flip(dims=[0])
-    rec_filters = torch.stack([rec_lo.unsqueeze(0) * rec_lo.unsqueeze(1),
-                               rec_lo.unsqueeze(0) * rec_hi.unsqueeze(1),
-                               rec_hi.unsqueeze(0) * rec_lo.unsqueeze(1),
-                               rec_hi.unsqueeze(0) * rec_hi.unsqueeze(1)], dim=0)
-
+    # reconstruction filters
+    rec_hi = torch.tensor(w.rec_hi[::-1], dtype=dtype).flip(dims=[0])
+    rec_lo = torch.tensor(w.rec_lo[::-1], dtype=dtype).flip(dims=[0])
+    rec_filters = torch.stack([
+        rec_lo.unsqueeze(0) * rec_lo.unsqueeze(1),
+        rec_lo.unsqueeze(0) * rec_hi.unsqueeze(1),
+        rec_hi.unsqueeze(0) * rec_lo.unsqueeze(1),
+        rec_hi.unsqueeze(0) * rec_hi.unsqueeze(1),
+    ], dim=0)
     rec_filters = rec_filters[:, None].repeat(out_size, 1, 1, 1)
 
     return dec_filters, rec_filters
 
+
 def wavelet_transform(x, filters):
     b, c, h, w = x.shape
-    pad = (filters.shape[2] // 2 - 1, filters.shape[3] // 2 - 1)
-    x = F.conv2d(x, filters, stride=2, groups=c, padding=pad)
+    pad = (filters.shape[3] // 2 - 1, filters.shape[3] // 2 - 1,
+           filters.shape[2] // 2 - 1, filters.shape[2] // 2 - 1)
+    # pad: (left, right, top, bottom)
+    x = F.pad(x, pad, mode='reflect')
+    x = F.conv2d(x, filters, stride=2, groups=c)
     x = x.reshape(b, c, 4, h // 2, w // 2)
     return x
 
 
 def inverse_wavelet_transform(x, filters):
     b, c, _, h_half, w_half = x.shape
-    pad = (filters.shape[2] // 2 - 1, filters.shape[3] // 2 - 1)
+    pad = (filters.shape[3] // 2 - 1, filters.shape[3] // 2 - 1,
+           filters.shape[2] // 2 - 1, filters.shape[2] // 2 - 1)
     x = x.reshape(b, c * 4, h_half, w_half)
-    x = F.conv_transpose2d(x, filters, stride=2, groups=c, padding=pad)
+    x = F.conv_transpose2d(x, filters, stride=2, groups=c)
+    # 反卷积输出比原图多一点边，可以在外面裁掉
     return x
 
 
@@ -58,7 +68,7 @@ def wavelet_transform_init(filters):
         @staticmethod
         def backward(ctx, grad_output):
             grad = inverse_wavelet_transform(grad_output, filters)
-            return grad, None
+            return grad
 
     return WaveletTransform().apply
 
@@ -75,13 +85,29 @@ def inverse_wavelet_transform_init(filters):
         @staticmethod
         def backward(ctx, grad_output):
             grad = wavelet_transform(grad_output, filters)
-            return grad, None
+            return grad
 
     return InverseWaveletTransform().apply
 
 
+class _ScaleModule(nn.Module):
+    def __init__(self, dims, init_scale=1.0):
+        super(_ScaleModule, self).__init__()
+        self.weight = nn.Parameter(torch.ones(*dims) * init_scale)
+
+    def forward(self, x):
+        return self.weight * x
+
+
 class WTConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=5, stride=1, bias=True, wt_levels=1, wt_type='db1'):
+    """
+    Wavelet convolution block:
+    - 多层小波分解 -> 在小波子带上做 depthwise conv -> 逆小波重建
+    - 再加一个 base depthwise conv 的残差
+    """
+    def __init__(self, in_channels, out_channels,
+                 kernel_size=5, stride=1,
+                 bias=True, wt_levels=1, wt_type='db1'):
         super(WTConv2d, self).__init__()
 
         assert in_channels == out_channels
@@ -89,42 +115,50 @@ class WTConv2d(nn.Module):
         self.in_channels = in_channels
         self.wt_levels = wt_levels
         self.stride = stride
-        self.dilation = 1
 
-        self.wt_filter, self.iwt_filter = create_wavelet_filter(wt_type, in_channels, in_channels, torch.float)
-        self.wt_filter = nn.Parameter(self.wt_filter, requires_grad=False)
-        self.iwt_filter = nn.Parameter(self.iwt_filter, requires_grad=False)
+        wt_filter, iwt_filter = create_wavelet_filter(
+            wt_type, in_channels, in_channels, torch.float32)
+        self.wt_filter = nn.Parameter(wt_filter, requires_grad=False)
+        self.iwt_filter = nn.Parameter(iwt_filter, requires_grad=False)
 
         self.wt_function = wavelet_transform_init(self.wt_filter)
         self.iwt_function = inverse_wavelet_transform_init(self.iwt_filter)
 
-        self.base_conv = nn.Conv2d(in_channels, in_channels, kernel_size, padding='same', stride=1, dilation=1,
-                                   groups=in_channels, bias=bias)
+        pad = kernel_size // 2
+        self.base_conv = nn.Conv2d(
+            in_channels, in_channels, kernel_size,
+            padding=pad, stride=1, dilation=1,
+            groups=in_channels, bias=bias)
         self.base_scale = _ScaleModule([1, in_channels, 1, 1])
 
-        self.wavelet_convs = nn.ModuleList(
-            [nn.Conv2d(in_channels * 4, in_channels * 4, kernel_size, padding='same', stride=1, dilation=1,
-                       groups=in_channels * 4, bias=False) for _ in range(self.wt_levels)]
-        )
-        self.wavelet_scale = nn.ModuleList(
-            [_ScaleModule([1, in_channels * 4, 1, 1], init_scale=0.1) for _ in range(self.wt_levels)]
-        )
+        self.wavelet_convs = nn.ModuleList([
+            nn.Conv2d(in_channels * 4, in_channels * 4, kernel_size,
+                      padding=pad, stride=1, dilation=1,
+                      groups=in_channels * 4, bias=False)
+            for _ in range(self.wt_levels)
+        ])
+        self.wavelet_scale = nn.ModuleList([
+            _ScaleModule([1, in_channels * 4, 1, 1], init_scale=0.1)
+            for _ in range(self.wt_levels)
+        ])
 
         if self.stride > 1:
-            self.stride_filter = nn.Parameter(torch.ones(in_channels, 1, 1, 1), requires_grad=False)
-            self.do_stride = lambda x_in: F.conv2d(x_in, self.stride_filter, bias=None, stride=self.stride,
-                                                   groups=in_channels)
+            self.stride_filter = nn.Parameter(
+                torch.ones(in_channels, 1, 1, 1), requires_grad=False)
+            self.do_stride = lambda x_in: F.conv2d(
+                x_in, self.stride_filter, bias=None,
+                stride=self.stride, groups=in_channels)
         else:
             self.do_stride = None
 
     def forward(self, x):
-
         x_ll_in_levels = []
         x_h_in_levels = []
         shapes_in_levels = []
 
         curr_x_ll = x
 
+        # 多层小波分解
         for i in range(self.wt_levels):
             curr_shape = curr_x_ll.shape
             shapes_in_levels.append(curr_shape)
@@ -132,11 +166,12 @@ class WTConv2d(nn.Module):
                 curr_pads = (0, curr_shape[3] % 2, 0, curr_shape[2] % 2)
                 curr_x_ll = F.pad(curr_x_ll, curr_pads)
 
-            curr_x = self.wt_function(curr_x_ll)
+            curr_x = self.wt_function(curr_x_ll)   # (B, C, 4, H/2, W/2)
             curr_x_ll = curr_x[:, :, 0, :, :]
 
             shape_x = curr_x.shape
-            curr_x_tag = curr_x.reshape(shape_x[0], shape_x[1] * 4, shape_x[3], shape_x[4])
+            curr_x_tag = curr_x.reshape(
+                shape_x[0], shape_x[1] * 4, shape_x[3], shape_x[4])
             curr_x_tag = self.wavelet_scale[i](self.wavelet_convs[i](curr_x_tag))
             curr_x_tag = curr_x_tag.reshape(shape_x)
 
@@ -145,6 +180,7 @@ class WTConv2d(nn.Module):
 
         next_x_ll = 0
 
+        # 多层逆小波重建
         for i in range(self.wt_levels - 1, -1, -1):
             curr_x_ll = x_ll_in_levels.pop()
             curr_x_h = x_h_in_levels.pop()
@@ -152,7 +188,8 @@ class WTConv2d(nn.Module):
 
             curr_x_ll = curr_x_ll + next_x_ll
 
-            curr_x = torch.cat([curr_x_ll.unsqueeze(2), curr_x_h], dim=2)
+            curr_x = torch.cat(
+                [curr_x_ll.unsqueeze(2), curr_x_h], dim=2)
             next_x_ll = self.iwt_function(curr_x)
 
             next_x_ll = next_x_ll[:, :, :curr_shape[2], :curr_shape[3]]
@@ -160,8 +197,8 @@ class WTConv2d(nn.Module):
         x_tag = next_x_ll
         assert len(x_ll_in_levels) == 0
 
-        x = self.base_scale(self.base_conv(x))
-        x = x + x_tag
+        x_base = self.base_scale(self.base_conv(x))
+        x = x_base + x_tag
 
         if self.do_stride is not None:
             x = self.do_stride(x)
@@ -169,20 +206,25 @@ class WTConv2d(nn.Module):
         return x
 
 
-class _ScaleModule(nn.Module):
-    def __init__(self, dims, init_scale=1.0, init_bias=0):
-        super(_ScaleModule, self).__init__()
-        self.dims = dims
-        self.weight = nn.Parameter(torch.ones(*dims) * init_scale)
-        self.bias = None
+class WaveletPreprocess(nn.Module):
+    """
+    简单封装：输入/输出形状不变 (B, 3, H, W)，只做一次 WTConv2d
+    """
+    def __init__(self,
+                 in_channels=3,
+                 kernel_size=5,
+                 wt_levels=1,
+                 wt_type='db1'):
+        super().__init__()
+        self.block = WTConv2d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=kernel_size,
+            wt_levels=wt_levels,
+            wt_type=wt_type,
+            stride=1,
+            bias=True,
+        )
 
     def forward(self, x):
-        return torch.mul(self.weight, x)
-
-
-if __name__ == '__main__':
-    block = WTConv2d(in_channels=3, out_channels=3)
-    input = torch.rand(1, 3, 64, 64)
-    output = block(input)
-    print(input.size())
-    print(output.size())
+        return self.block(x)
